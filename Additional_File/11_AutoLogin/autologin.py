dd55cd561d7fe4e7d662ece2ad83ecc02d37d73f
AstraaDev <43183806+AstraaDev@users.noreply.github.com>
2021-10-11 18:31:12 +0200

    Add files via upload

--- Additional_File/11_AutoLogin/autologin.py
+++ Additional_File/11_AutoLogin/autologin.py
@@ -2,12 +2,13 @@ import os, sys, time, os.path, pyperclip, pyautogui, ctypes
 from colorama import Fore
 from selenium import webdriver
 
-def main() :   
-    ctypes.windll.kernel32.SetConsoleTitleW("AutoLogin - Made by Astraa")
-    autologin()
-    print(f"""{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTWHITE_EX }+{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Enter the token of the account you want to connect to: """)
+def autologin() :
+    os.system(f'title AutoLogin - Made by Astraa')
+    os.system('cls')
+    autologintitle()
+    print(f"""{y}[{w}+{y}]{w} Enter the token of the account you want to connect to: """)
     print()
-    entertoken = str(input(f"""{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTBLUE_EX }#{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Token: """))
+    entertoken = str(input(f"""{y}[{b}#{y}]{w} Token: """))
     print("\n\n")
     if len(entertoken) >= 59:
         driver = webdriver.Chrome(executable_path=r'Additional_File/11_AutoLogin/chromedriver.exe')
@@ -16,18 +17,22 @@ def main() :
         js = 'function login(token) {setInterval(() => {document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`}, 50);setTimeout(() => {location.reload();}, 500);}'
         time.sleep(3)
         driver.execute_script(js + f'login("{entertoken}")')
-        time.sleep(5)
-        if driver.current_url == 'https://discord.com/channels/@me':
-            print(f"""\n\n\n\n{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTGREEN_EX }!{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Connection Established""")
-        else:
-            print(f"""\n{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTRED_EX }!{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Connection Failed""")
+        time.sleep(10)
+        if driver.current_url == 'https://discord.com/login':
+            os.system('cls')
+            autologintitle()
+            print(f"""{y}[{Fore.LIGHTRED_EX }!{y}]{w} Connection Failed""")
             driver.close()
-        input(f"""{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTBLUE_EX }#{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Press ENTER to exit""")
-        exit(0)
+        else:
+            os.system('cls')
+            autologintitle()
+            print(f"""{y}[{Fore.LIGHTGREEN_EX }!{y}]{w} Connection Established""")
+        input(f"""{y}[{b}#{y}]{w} Press ENTER to exit""")
+        main()
     else:
-        print(f"""      {Fore.LIGHTYELLOW_EX }[{Fore.LIGHTRED_EX }!{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } There is a problem with your Token.""")
+        print(f"""      {y}[{Fore.LIGHTRED_EX }!{y}]{w} There is a problem with your Token.""")
         time.sleep(2)
         os.system('cls')
-        exit(0)
+        main()
 
-main()
+autologin()
