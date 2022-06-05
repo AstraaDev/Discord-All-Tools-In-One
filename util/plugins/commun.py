0e05c7d85c7084f981ad42e801c3c005323c15f9
a5traa <43183806+AstraaDev@users.noreply.github.com>
2022-06-05 20:42:40 +0200

    Added "Server Lookup" option
    Added "Server Lookup" option + Improved "Settings Cycler

--- util/plugins/commun.py
+++ util/plugins/commun.py
@@ -4,7 +4,7 @@ from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
 from colorama import Fore
 from time import sleep
 
-THIS_VERSION = "1.1.7"
+THIS_VERSION = "1.1.8"
 
 y = Fore.LIGHTYELLOW_EX
 b = Fore.LIGHTBLUE_EX
