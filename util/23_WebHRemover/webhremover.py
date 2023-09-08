19e1b6820208b178792ef39ecc756b4b99d0a3dc
a5traa <43183806+AstraaDev@users.noreply.github.com>
2023-09-08 17:02:42 +0200

    Update webhremover.py

--- util/23_WebHRemover/webhremover.py
+++ util/23_WebHRemover/webhremover.py
@@ -9,15 +9,15 @@ webhremovertitle()
 
 def webhooksremover():
     try:
-        print(f"""{y}[{w}+{y}]{w} Enter the WebHook you want to delete """)
+        print(f"{y}[{w}+{y}]{w} Enter the WebHook you want to delete ")
         webhook = input(f"""{y}[{b}#{y}]{w} WebHook Link: """)
         requests.delete(webhook.rstrip())
-        print(f"""\n{y}[{Fore.LIGHTGREEN_EX }!{y}]{w} Webhook has been deleted""")
-        input(f"""\n{y}[{b}#{y}]{w} Press ENTER to exit""")
+        print(f"\n{y}[{Fore.LIGHTGREEN_EX }!{y}]{w} Webhook has been deleted")
+        input(f"\n{y}[{b}#{y}]{w} Press ENTER to exit")
         main()
     except:
-        print(f"""\n{y}[{Fore.LIGHTRED_EX }!{y}]{w} Webhook could not be deleted""")
-        input(f"""\n{y}[{b}#{y}]{w} Press ENTER to exit""")
+        print(f"\n{y}[{Fore.LIGHTRED_EX }!{y}]{w} Webhook could not be deleted")
+        input(f"\n{y}[{b}#{y}]{w} Press ENTER to exit")
         main()
           
 webhooksremover()
